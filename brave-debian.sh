78ba662d91a3e9844b3b74409dfed12fd74739a1
TheTapo <93761910+TheTapo@users.noreply.github.com>
2022-03-21 14:07:38 +0530

    Create brave-debian.sh

---
+++ brave-debian.sh
@@ -0,0 +1,17 @@
+echo you have executed this file shall i install brave-browser for you?
+echo press y to continue or press enter to stop
+read name
+if [ $name ];
+then
+sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+else
+exit
+fi
