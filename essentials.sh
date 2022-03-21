6b4eed8f59772477d71398ce5474d913d6e7bcac
TheTapo <93761910+TheTapo@users.noreply.github.com>
2022-03-21 14:21:49 +0530

    Create essentials.sh
    this is my essentials script. this script installs discord minecraft launcher brave browser and cadmus.

---
+++ essentials.sh
@@ -0,0 +1,38 @@
+echo shall i start?
+read name
+echo press y to continue or press enter to stop
+if [ $name ];
+then 
+sudo apt install wget
+cd ~
+cd Downloads
+wget https://github.com/josh-richardson/cadmus/releases/download/0.0.3/cadmus.deb
+sudo dpkg --install cadmus.deb
+echo installed cadmus
+
+sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+
+echo brave browser installed
+
+wget https://launcher.mojang.com/download/Minecraft.deb
+
+sudo dpkg --install Minecraft.deb
+
+echo installed minecraft
+
+wget https://discord.com/api/download?platform=linux&format=deb
+
+sudo dpkg --install discord-0.0.17.deb
+else
+
+exit
+
+fi
